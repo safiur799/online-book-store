7b8bf4745a6f44d9ebe6900cd0257729c153d481
safiur799 <safiurrahaman799@gmail.com>
2021-09-27 18:09:39 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import SearchBar from "./components/SearchBar";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import BookDetails from "./components/BookDetails";
 function App() {
   return (
     <div className="App">
       <h1>Online book api</h1>
-      <SearchBar />
+      <Navbar />
+      <Switch>
+        <Route path="/books" exact>
+          <SearchBar />
+        </Route>
+        <Route path="/books/:bookId">
+          <BookDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <div>
+      <Link to="/books">books</Link>
+    </div>
+  );
+}
+export default Navbar;
