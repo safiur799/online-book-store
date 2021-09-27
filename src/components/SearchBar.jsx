32b8cdcd1cb56642b122ce9395f6ab6129689a8a
safiur799 <safiurrahaman799@gmail.com>
2021-09-27 16:46:23 +0530

    databaseAdd

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function SearchBar() {
+  const [book, setBook] = useState([]);
+  const [page, setPage] = useState(0);
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  function getBooks() {
+    fetch(`http://localhost:8000/books?_page=${page}&_limit=10`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log("books", data);
+        setBook(data);
+      });
+  }
+
   return (
     <div>
       <input type="text" placeholder="search book " />
