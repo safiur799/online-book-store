509bcefb467a216d79c2aa4050103dffca821522
safiur799 <safiurrahaman799@gmail.com>
2021-09-27 16:00:38 +0530

    firstCommit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="search book " />
+      <button type="submit">search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
